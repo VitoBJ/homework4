4c8cfb408c04dd54eaad7a68cf6f8876f9b1f25d
VitoBJ <Shizane7@gmail.com>
2024-07-27 01:09:01 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,15 @@
+my_string=input('Введите ваш номер телефона')
+my_string = "Введите номер телефона"
+length = len(my_string)
+print(length)
+print('Введите ваш номер телефона'.upper())
+print('Введите ваш номер телефона'.lower())
+print('Введите ваш номер телефона'.replace(' ', ''))
+print('my_string'[0])
+print('my_string'[8])
+my_string_two = 'Введите смысл Вашей жизни'
+length = len(my_string_two)
+print(length)
+
+
+
